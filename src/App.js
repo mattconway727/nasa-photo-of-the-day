8354fc1f98648c29eda0180e9f2f0d9389a24393
Matt Conway <mattconway727@gmail.com>
2019-10-24 22:04:44 -0400

    mvp minus styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import ImageStuff from "./ImageThing";
 
 function App() {
+  const [stuff, setStuff] = useState({});
+
+  useEffect(() => {
+    console.log("yay");
+
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(res => {
+        console.log("nasa res");
+        setStuff(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className="stuff">
+        <h1>Stuff</h1>
+        <p>{stuff.copyright}</p>
+        <ImageStuff stuff={stuff} />
+      </div>
     </div>
   );
 }
